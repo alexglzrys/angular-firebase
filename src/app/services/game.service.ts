d9c7f8464b46c664c449886ec03d2182fe637918
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-26 14:54:28 -0600

    mostrar juegos en plantilla

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from "rxjs/operators";
 import { environment } from 'src/environments/environment';
 import { Game } from '../shared/interfaces/game';
 
@@ -11,10 +12,22 @@ const API_URL = environment.api;
 })
 export class GameService {
 
+  private games: Game[] = [];
+
   constructor(private http: HttpClient) { }
 
   getNominados(): Observable<Game[]> {
-    const URL = `${API_URL}/goty`;
-    return this.http.get<Game[]>(URL);
+    if (this.games.length > 0) {
+      // Retornar un observable con los juegos almacenados en la propiedad privada games
+      console.log('Games desde Caché')
+      return of(this.games);
+    } else {
+      // Hacer petición http y como tarea secundaria almacenar el resultado en la propiedad privada games (Caché)
+      const URL = `${API_URL}/goty`;
+      console.log('Games desde HTTPS')
+      return this.http.get<Game[]>(URL).pipe(
+        tap(games => this.games = games)
+      );
+    }
   }
 }
