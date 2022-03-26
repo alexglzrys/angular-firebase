5578af5268fb73252f7df0f09de363ac9596f20c
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-26 16:45:09 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-firebase-chart',
-    appId: '1:52442432718:web:e0d3cb08d46685893e3c9f',
-    storageBucket: 'angular-firebase-chart.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyCa5xK2uQNFhjEHR-jxon8RVoBoqGRwj-Y',
-    authDomain: 'angular-firebase-chart.firebaseapp.com',
-    messagingSenderId: '52442432718',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
   production: true,
   api: 'http://localhost:5000/angular-firebase-chart/us-central1/api'
